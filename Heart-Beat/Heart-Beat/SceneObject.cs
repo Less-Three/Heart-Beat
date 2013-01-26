6cfc70312f6fa646c60603c4dff29c783698b0cb
Daniel McKerricher <dmckerricher@hotmail.com>
2013-01-26 01:27:39 -0800

    Base classes
    added sceneobject and basic extending classes
    declared basic variables in sceneobject

---
+++ Heart-Beat/Heart-Beat/SceneObject.cs
@@ -0,0 +1,54 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+
+
+namespace Heart_Beat
+{
+    /// <summary>
+    /// This is a game component that implements IUpdateable.
+    /// </summary>
+    public class SceneObject : Microsoft.Xna.Framework.GameComponent
+    {
+        private Vector3 location; // x = left right, y= near far, z = up down
+        //private List<Animation> animations;
+        private int hitpoints;
+        private Rectangle collisionRectangle; // x and z collision
+        private double collisionWidth; // y width of collision
+
+        public SceneObject(Game game)
+            : base(game)
+        {
+            // TODO: Construct any child components here
+        }
+
+        /// <summary>
+        /// Allows the game component to perform any initialization it needs to before starting
+        /// to run.  This is where it can query for any required services and load content.
+        /// </summary>
+        public override void Initialize()
+        {
+            // TODO: Add your initialization code here
+
+            base.Initialize();
+        }
+
+        /// <summary>
+        /// Allows the game component to update itself.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        public override void Update(GameTime gameTime)
+        {
+            // TODO: Add your update code here
+
+            base.Update(gameTime);
+        }
+    }
+}
