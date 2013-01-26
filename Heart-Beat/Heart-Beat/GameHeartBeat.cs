435f0e56a12c998542ede73d5f7db46cb3c81b2b
Johnathan Chu <jtychu@outlook.com>
2013-01-26 00:15:58 -0800

    Renamed Game1.cs to GameHeartBeat.cs

--- Heart-Beat/Heart-Beat/GameHeartBeat.cs
+++ Heart-Beat/Heart-Beat/GameHeartBeat.cs
@@ -14,12 +14,12 @@ namespace Heart_Beat
     /// <summary>
     /// This is the main type for your game
     /// </summary>
-    public class Game1 : Microsoft.Xna.Framework.Game
+    public class GameHeartBeat : Microsoft.Xna.Framework.Game
     {
         GraphicsDeviceManager graphics;
         SpriteBatch spriteBatch;
 
-        public Game1()
+        public GameHeartBeat()
         {
             graphics = new GraphicsDeviceManager(this);
             Content.RootDirectory = "Content";
